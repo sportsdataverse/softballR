482641556cbb82e5ec787d027bf1ee4feda815be
tmking2002 <tmking4@ncsu.edu>
2023-05-10 15:39:04 -0400

    Update ncaa_softball_teams.R

--- R/ncaa_softball_teams.R
+++ R/ncaa_softball_teams.R
@@ -1,28 +1,18 @@
 #' Get all NCAA softball team names and ids
 #'
 #' @author Tyson King
-#' @description Only has division 1 teams for now
+#' @description 2021-2023 for D1, only 2023 for D2 and D3
 #' @param season 2021, 2022 or 2023
+#' @param division "D1", "D2", or "D3
 #'
 #' @return data frame of date, team names and their scores
-#' @importFrom dplyr case_when filter select mutate
-#' @importFrom glue glue
-#' @importFrom stringr str_remove_all
-#' @importFrom magrittr extract extract2
+#' @importFrom dplyr case_when filter select distinct
 #' @importFrom rvest read_html html_table
 #' @export
 #'
 #' @examples
 #' try(ncaa_softball_teams(2023))
-ncaa_softball_teams <- function(season){
-
-  possible_seasons <- c(2021, 2022, 2023)
-
-  if(!(season %in% possible_seasons)) stop("Invalid Season")
-
-  url_ext <- dplyr::case_when(season == 2021 ~ 21123,
-                              season == 2022 ~ 27203,
-                              season == 2023 ~ 30768)
+ncaa_softball_teams <- function(season, division = "D1"){
 
   team_ids <- "https://stats.ncaa.org/game_upload/team_codes" %>%
     rvest::read_html() %>%
@@ -31,38 +21,36 @@ ncaa_softball_teams <- function(season){
     dplyr::filter(!(X1 %in% c("NCAA Codes", "ID"))) %>%
     `names<-`(c("team_id", "team_name"))
 
-  raw_softball <- glue::glue("https://stats.ncaa.org/selection_rankings/nitty_gritties/{url_ext}") %>%
-    readLines()
+  possible_seasons <- c(2021, 2022, 2023)
+  possible_divisions <- c("D1", "D2", "D3")
 
-  locs <- grep("<a target=\"TEAM_PAGE\"", raw_softball)
+  if(!(season %in% possible_seasons &
+       division %in% possible_divisions)) stop("Invalid Season")
 
-  teams_raw <- raw_softball[locs]
-  teams <- data.frame()
-
-  for(i in 1:length(teams_raw)){
-
-    split <- strsplit(teams_raw[i], "href=\"/teams/|\"") %>%
-      magrittr::extract2(1)
-
-    softball_id <- split %>%
-      magrittr::extract(14)
-
-    team_name <- split %>%
-      magrittr::extract(15) %>%
-      stringr::str_remove_all(">|</a></td>|&#39|&amp;")
-
-    teams <- rbind(teams, data.frame(team_name, softball_id))
-
-  }
-
-  teams <- merge(teams, team_ids, by = "team_name") %>%
-    dplyr::mutate(season = season)
+  if(division %in% c("D2","D3") & season != 2023) stop("Invalid Season")
 
   team_info <- load_ncaa_softball_team_info()
 
-  teams_final <- merge(teams, team_info, by = c("team_id","season")) %>%
-    dplyr::select(team_name, team_id, softball_id, division, conference, head_coach, wins, losses, ties, win_perc)
+  division_id <- dplyr::case_when(division == "D1" ~ "D-I",
+                                  division == "D2" ~ "D-II",
+                                  division == "D3" ~ "D-III")
 
-  return(teams_final)
+  season_id <- season
+
+  teams <- team_info %>%
+    dplyr::filter(division == division_id &
+                    season == season_id)
+
+  scoreboard <- load_ncaa_softball_scoreboard(season = season,
+                                              division = division)
+
+  softball_ids <- rbind(scoreboard[c(1,2)] %>% `names<-`(c("team_name","softball_id")),
+                        scoreboard[c(5,6)] %>% `names<-`(c("team_name","softball_id"))) %>%
+    dplyr::distinct()
+
+  teams_final <- merge(teams, team_ids, by = "team_id") %>%
+    merge(softball_ids, by = "team_name") %>%
+    dplyr::select(season, team_name, team_id, softball_id, division, conference,
+                  head_coach, wins, losses, ties, win_perc)
 
 }
