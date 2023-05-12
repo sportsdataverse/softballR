270312c9bc3e8f4f43542ae41c412838f609cd5c
tmking2002 <tmking4@ncsu.edu>
2023-05-12 15:43:26 -0400

    1.0.2
    can now pass multiple years as arguments in load scoreboard function

--- R/load_ncaa_softball_scoreboard.R
+++ R/load_ncaa_softball_scoreboard.R
@@ -1,36 +1,65 @@
 #' Load NCAA Scoreboard
 #'
-#' @description Supports 2019-2023 for D1 and only 2023 for D2 and D3
-#' @param season YYYY
+#' @description Supports 2016-2023 for all divisions
+#' @param season YYYY format, pass season or vector of seasons
 #' @param division "D1", "D2", or "D3"
 #'
 #' @return dataframe of all games from NCAA website from selected season
 #' @importFrom glue glue
 #' @export
 #'
-#' @examples try(load_ncaa_softball_scoreboard(2023))
+#' @examples try(load_ncaa_softball_scoreboard(2022:2023))
 load_ncaa_softball_scoreboard <- function(season, division = "D1"){
 
   if(!is.numeric(season)) return("Invalid Input")
 
   if(!(division %in% c("D1", "D2", "D3"))) stop("Invalid Division")
 
-  if((division != "D1" & season != 2023)) stop("Invalid Date")
+  if(min(season) < 2016 | max(season) > 2023) stop("Invalid Season")
 
-  if(division == "D1"){
+  if(length(season) == 1){
 
-    url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/ncaa_scoreboard_{season}.RDS?raw=true")
+    if(division == "D1"){
+
+      url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/ncaa_scoreboard_{season}.RDS?raw=true")
+
+    } else{
+
+      url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/ncaa_scoreboard_{division}_{season}.RDS?raw=true")
+
+    }
+
+    con <- url(url)
+
+    on.exit(close(con))
+
+    scoreboard <- try(readRDS(con), silent = TRUE)
 
   } else{
 
-    url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/ncaa_scoreboard_{division}_{season}.RDS?raw=true")
+    scoreboard <- data.frame()
+
+    for(i in 1:length(season)){
+
+      if(division == "D1"){
+
+        url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/ncaa_scoreboard_{season[i]}.RDS?raw=true")
+
+      } else{
+
+        url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/ncaa_scoreboard_{division}_{season[i]}.RDS?raw=true")
+
+      }
+
+      con <- url(url)
+
+      on.exit(close(con))
+
+      scoreboard <- rbind(scoreboard, try(readRDS(con), silent = TRUE))
+    }
 
   }
 
-  con <- url(url)
-
-  on.exit(close(con))
-
-  scoreboard <- try(readRDS(con), silent = TRUE)
+  return(scoreboard)
 
 }
