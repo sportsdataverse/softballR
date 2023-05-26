7b86b7ddf8db599889c1653af4dbc5b0d9ac0d8b
tmking2002 <tmking4@ncsu.edu>
2023-05-26 15:12:16 -0400

    added functionality for 2022 and 2023 player box data

--- R/load_ncaa_softball_playerbox.R
+++ R/load_ncaa_softball_playerbox.R
@@ -1,7 +1,7 @@
 #' Load NCAA Player Box Scores
 #'
-#' @description Only supports 2023 for now (only D1 for fielding)
-#' @param season YYYY (2023)
+#' @description 2021-2023 for D1 hitting and pitching, only 2023 for everything else
+#' @param season YYYY can also use vector of years
 #' @param category Hitting, Pitching, or Fielding
 #' @importFrom glue glue
 #' @importFrom stringr str_replace
@@ -9,26 +9,34 @@
 #' @return dataframe of all box scores from NCAA website from selected season
 #' @export
 #'
-#' @examples try(load_ncaa_softball_playerbox(2023))
+#' @examples try(load_ncaa_softball_playerbox(2021:2023, category = "Hitting"))
 load_ncaa_softball_playerbox <- function(season = 2023, category, division = "D1"){
 
   if(!is.numeric(season)) return("Invalid Season")
 
   if(!(category %in% c("Hitting", "Pitching", "Fielding"))) return("Invalid Category")
 
-  if(season != 2023) return("Only includes 2023 data... for now")
+  if((season[1] != "2023" | length(season > 1)) & (division != "D1" | category == "Fielding")) return("Only includes 2023 data... for now")
+
+  if(min(season < 2021 | max(season > 2023))) return("Invalid Season")
 
   if(!(division %in% c("D1", "D2", "D3"))) stop("Invalid Division")
 
   division <- stringr::str_replace(division, "D", "d")
 
+  url <- c()
+
   if(category == "Hitting"){
 
-    url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/{division}_hitting_box_scores_2023.RDS?raw=true")
+    for(i in season){
+      url <- c(url, glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/{division}_hitting_box_scores_{i}.RDS?raw=true"))
+    }
 
   } else if(category == "Pitching"){
 
-    url <- glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/{division}_pitching_box_scores_2023.RDS?raw=true")
+    for(i in season){
+      url <- c(url, glue::glue("https://github.com/tmking2002/softballR-data/blob/main/data/{division}_pitching_box_scores_{i}.RDS?raw=true"))
+    }
 
   } else if(category == "Fielding"){
 
@@ -36,11 +44,17 @@ load_ncaa_softball_playerbox <- function(season = 2023, category, division = "D1
 
   }
 
-  con <- url(url)
+  box <- data.frame()
 
-  on.exit(close(con))
+  for(i in url){
 
-  box <- try(readRDS(con), silent = TRUE)
+    con <- url(i)
+
+    on.exit(close(con))
+
+    box <- bind_rows(box, readRDS(con))
+
+  }
 
   return(box)
 
